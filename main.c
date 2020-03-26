ecb75d49bebd4c38e5b4bb75f0e8c84e16f908f3
Fabricio Henrique <fnhs27@gmail.com>
2020-03-26 19:21:06 -0300

    Arquivo secundario para testes

--- main.c
+++ main.c
@@ -3,9 +3,7 @@
     Aluno de Analise e Desenvolvimento de Sistemas
     Rede Neural Perceptron
     Data: 20/03/2020
-    Atualização: 26/03/2020
-
-    testando Git
+    Atualização: 24/03/2020
 
 **/
 
@@ -14,10 +12,11 @@
 #include <math.h>
 
 void exibirMatriz(int nlin, int ncol, int *ptr_matriz);
-int *alocaMatriz(int num_entradas, int num_neuronios);
+int *alocaMatriz(int nlin, int ncol);
 float sigmoid(int *matriz, int nlin);
 float randomico(void);
-int *produtoMatriz(int *mat1, int lin_mat1, int col_mat1, int *mat2, int lin_mat2, int col_mat2);
+int *produtoMatriz(int *mat1, int nlin_mat1, int ncol_mat1, int *mat2, int nlin_mat2, int ncol_mat2);
+int *preencher(int *matriz, int nlin, int ncol);
 
 float sigmoid(int *matriz, int nlin){
 
@@ -35,24 +34,24 @@ float sigmoid(int *matriz, int nlin){
 
 float randomico(void){
 
-    srand((unsigned int)time(0));
+
 
     return ((float)(rand()/(float)RAND_MAX))*10;
 
 }
 
-int *produtoMatriz(int *mat1, int lin_mat1, int col_mat1, int *mat2, int lin_mat2, int col_mat2){
+int *produtoMatriz(int *mat1, int nlin_mat1, int ncol_mat1, int *mat2, int nlin_mat2, int ncol_mat2){
 
     int *prod_mat, i, j, soma = 0;
 
-    prod_mat = alocaMatriz(lin_mat1, col_mat2);
+    prod_mat = alocaMatriz(nlin_mat1, ncol_mat2);
 
-    if(col_mat1 == lin_mat2){
+    if(ncol_mat1 == nlin_mat2){
 
-        for(i = 0; i < lin_mat1; i++){
+        for(i = 0; i < nlin_mat1; i++){
             soma = 0;
-            for(j = 0; j < lin_mat2; j++){
-                soma = soma + (*(mat1 + (i * col_mat1) + j) * *(mat2 + j));
+            for(j = 0; j < nlin_mat2; j++){
+                soma = soma + (*(mat1 + (i * ncol_mat1) + j) * *(mat2 + j));
             }
             *(prod_mat+i) = soma;
         }
@@ -60,11 +59,9 @@ int *produtoMatriz(int *mat1, int lin_mat1, int col_mat1, int *mat2, int lin_mat
     return prod_mat;
 }
 
-int *alocaMatriz(int num_entradas, int num_neuronios){
+int *alocaMatriz(int nlin, int ncol){
 
-    int i, *ponteiro, *q, nlin, ncol;
-    nlin = num_entradas;
-    ncol = num_neuronios;
+    int i, *ponteiro, *q;
 
     if(nlin < 1 || ncol < 1)
         printf("\n2. ERRO: Parametro de tamanho invalido.\n");
@@ -96,49 +93,87 @@ void exibirMatriz(int nlin, int ncol, int *ptr_matriz){
     }
 }
 
+int *preencher(int *matriz, int nlin, int ncol){
+
+    for(int i = 0; i < (nlin*ncol); i++){
+        *(matriz+i) = rand()%10;
+    }
+
+    return matriz;
+}
+
+void treinar(){
+
+    int yd[] = {0, 1, 1, 0};    //valores desejados
+    int neuronio = 3, entrada = 2;
+    float erro = 0.001;
+    float *pesos, *v;
+    int j;
+
+    int amostra1[] = {0, 0};
+    int amostra2[] = {0, 1};
+    int amostra3[] = {1, 0};
+    int amostra4[] = {1, 1};
+    int amostra;
+
+    int amostras[] = {0, 0, 1, 0, 0, 1, 1, 1};
+
+    pesos = alocaMatriz(neuronio, entrada);
+    pesos = preencher(pesos, neuronio, entrada);
+
+    printf("\nMatriz de pesos: \n");
+    exibirMatriz(neuronio, entrada, pesos);
+    printf("\n");
+
+    int r = 2*(rand()%4);
+
+    printf("\nMatriz de entrada: \nr = %d\n", r);
+    for(int i = r; i < r+2; i++){
+        amostra[j] = amostras[i];
+        printf("%d\n", amostra[i]);
+
+    }
+    printf("\n");
+
+    v = produtoMatriz(pesos, neuronio, entrada, &amostra, entrada, 1);
+    printf("\nMatriz resultado: \n");
+    exibirMatriz(neuronio, 1, v);
+
+
+
+
+}
+
 int main(){
 
-    char url1[] = "matrizpesos.txt";
-    char url2[] = "matrizentrada.txt";
-    FILE *arq, *input;
-    float yk;
-    int i, entrada = 5, neuronio = 4;  //entrada = dado de entrada, neuronio = numero de neuronios na rede neural
+    srand((unsigned int)time(NULL));
+
+    float yo;
+    int num_entradas = 2, num_neuronios = 3, num_saida = 1;
     int *weigth, *x_input, *u;
 
-    weigth = alocaMatriz(neuronio, entrada);
-    x_input = alocaMatriz(entrada, 1);
+    treinar();
+    exit(1);
 
-    arq = fopen(url1, "r");
-    input = fopen(url2, "r");
+    weigth = alocaMatriz(num_neuronios, num_entradas);
+    x_input = alocaMatriz(num_entradas, 1);
 
-    if(arq == NULL){
-        printf("\n1. ERRO ao abrir arquivos!\n");
-    }
-    else{
-        printf("\n1. Arquivos aberto com sucesso!\n\n");
-    }
+    weigth = preencher(weigth, num_neuronios, num_entradas);
 
-    for(i = 0; i < (entrada*neuronio); i++)
-        fscanf(arq, "%d", (weigth+i));
 
-    for(i = 0; i < entrada; i++)
-        fscanf(input, "%d", (x_input+i));
-
-    exibirMatriz(neuronio, entrada, weigth);
+    exibirMatriz(num_neuronios, num_entradas, weigth);
     printf("\n");
-    exibirMatriz(entrada, 1, x_input);
+    exibirMatriz(num_entradas, 1, x_input);
     printf("\n");
 
-    u = produtoMatriz(weigth, neuronio, entrada, x_input, entrada, 1);
-    exibirMatriz(neuronio, 1, u);
+    u = produtoMatriz(weigth, num_neuronios, num_entradas, x_input, num_entradas, 1);
+    exibirMatriz(num_neuronios, 1, u);
 
     printf("\n");
 
-    yk = sigmoid(u, neuronio);
-    printf("\nSaida: %f\n", yk);
+    yo = sigmoid(u, num_neuronios);
+    printf("\nSaida: %f\n", yo);
 
-    fclose(arq);
-    fclose(x_input);
     free(weigth);
     free(x_input);
 
