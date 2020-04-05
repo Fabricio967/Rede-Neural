bcdecba6dbcc47256764ebf18892c19148ba38cc
Fabricio Henrique <fnhs27@gmail.com>
2020-04-05 12:00:16 -0300

    clean code

--- main.c
+++ main.c
@@ -22,10 +22,8 @@ const float Erro_desejado = 0.01;
 
 void treinar(){
 
-    //int yd[] = {0, 1, 1, 0};    //saida desejados
     float *y_d_saida = alocaMatriz(numSaida, 1);
     float *y_d_oculta = alocaMatriz(numNeuronio, 1);
-    //int pesos[][] =
 
     for(int i = 0; i < numSaida; i++){
         *(y_d_saida+i) = 1;
@@ -49,100 +47,51 @@ void treinar(){
         }
     }
 
-    printf("\nMatriz de pesos:\n");
-    exibirMatriz(pesosOculta, numNeuronio, numEntrada);
-
-    printf("\nMatriz de entrada:\n");
-    exibirMatriz(entradas, numEntrada, 1);
-
     float *uOculta = produtoMatriz(pesosOculta, numNeuronio, numEntrada, entradas, numEntrada, 1);
     float *biasOculta = preencher(alocaMatriz(numNeuronio, 1), numNeuronio, 1);
 
-    system("pause");
-
-    printf("\nMatriz biasOculta:\n");
-    exibirMatriz(biasOculta, numNeuronio, 1);
+    //system("pause");
 
     uOculta = somaMatrizes(uOculta, numNeuronio, 1, biasOculta, numNeuronio, 1);
 
-    printf("\nMatriz uOculta:\n");
-    exibirMatriz(uOculta, numNeuronio, 1);
-
     float *yOculta = sigmoid(uOculta, numNeuronio);
-
-    printf("\nY oculta\n");
-    exibirMatriz(yOculta, numNeuronio, 1);
-
-    printf("\n-------------------------------------------------------------\n");
 
     //Camada Oculta -> Camada de Saida
     float *camadaSaida = yOculta;
-    printf("\nMatriz de entrada para neuronio de Saida: \n");
-    exibirMatriz(camadaSaida, numNeuronio, numSaida);
 
     float *pesosSaida = preencher(alocaMatriz(numSaida, numNeuronio), numSaida, numNeuronio);
     float *biasSaida = preencher(alocaMatriz(numSaida, numSaida), numSaida, numSaida);
     float *uSaida = produtoMatriz(pesosSaida, numSaida, numNeuronio, camadaSaida, numNeuronio, 1);
 
-    printf("\nMatriz uSaida:\n");
-    exibirMatriz(uSaida, numSaida, 1);
-
     uSaida = somaMatrizes(uSaida, numSaida, 1, biasSaida, numSaida, numSaida);
     float *ySaida = sigmoid(uSaida, numSaida);
-
-    printf("\nMatriz de pesos de saida:\n");
-    exibirMatriz(pesosSaida, numSaida, numNeuronio);
-
-    printf("\nMatriz biasSaida:\n");
-    exibirMatriz(biasSaida, numSaida, 1);
-
-    printf("\nMatriz uSaida:\n");
-    exibirMatriz(uSaida, numSaida, 1);
-
-    printf("\nY Saida\n");
-    exibirMatriz(ySaida, numSaida, 1);
-
-    printf("\n");
 
     //BACKPROPAGATION
 
     //Saida -> Oculta
 
     float *erro_saida = subtraiMatriz(y_d_saida, numSaida, 1, ySaida, numSaida, 1);
-    printf("\nErro na saida\n");
-    exibirMatriz(erro_saida, numSaida, 1);
 
+/*
     if(*(erro_saida) > Erro_desejado){
         printf("\n\t\ttrue\n");
-    }
+    }*/
 
     float *gradiente_os = produtoMatriz(camadaSaida, numNeuronio, 1, erro_saida, numSaida, 1);
-    printf("\ncamada gradiente_os\n");
     gradiente_os = produtoEscalar(gradiente_os, numNeuronio, 1, taxa_aprendizado);
     gradiente_os = transporMatriz(gradiente_os, numNeuronio, 1);
-    printf("\n");
-    exibirMatriz(gradiente_os, 1, numNeuronio);
-    //gradiente_os = transporMatriz(gradiente_os, numNeuronio, 1);
     pesosSaida = somaMatrizes(pesosSaida, numSaida, numNeuronio, gradiente_os, 1, numNeuronio);
-    printf("\n");
-    exibirMatriz(pesosSaida, numSaida, numNeuronio);
 
     //Oculta -> Entrada
-    printf("\n------------------------------\n");
+
     float *erro_oculta = produtoMatriz(entradas, numEntrada, 1, pesosSaida, 1, numNeuronio);
-    exibirMatriz(erro_oculta, 3, 2);
-    system("pause");
+
+    //system("pause");
 
     float *gradiente_eo = transporMatriz(erro_oculta, numEntrada, numNeuronio);
     gradiente_eo = produtoEscalar(gradiente_eo, numNeuronio, numEntrada, *erro_saida);
     gradiente_eo = produtoEscalar(gradiente_eo, numNeuronio, numEntrada, taxa_aprendizado);
     pesosOculta = somaMatrizes(pesosOculta, numNeuronio, numEntrada, gradiente_eo, numNeuronio, numEntrada);
-
-    printf("\ncamada gradiente_eo\n");
-    exibirMatriz(gradiente_eo, numNeuronio, numEntrada);
-    printf("\n");
-    printf("\n");
-    exibirMatriz(pesosOculta, numNeuronio, numEntrada);
 
     free(pesosOculta);
     free(biasOculta);
