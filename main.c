0b20fa88f3044026a2c27854b25fef4651052638
Fabricio Henrique <fnhs27@gmail.com>
2020-04-21 16:01:39 -0300

    Rede Neural que avalia emprestimos

--- main.c
+++ main.c
@@ -35,8 +35,8 @@ float amostras[NUMAMOSTRAS][NUMENTRADAS];
 
 float sigmoide(float z);
 float dsig(float z);
-float rnd();
 void preencherMatrizes();
+int salvarPesos();
 
 void preencherMatrizes(){
 
@@ -71,9 +71,6 @@ void treinar(){
     int i, j, k, numEpocas = 0;
     float soma = 0;
 
-    //float amostras[4][2] = { {0, 0}, {0, 1}, {1, 0}, {1, 1} };
-
-
     arqAmostras = fopen("amostras.txt", "r");
     if(!arqAmostras){
         printf("\nErro ao abrir arquivo de amostras.\n");
@@ -87,25 +84,21 @@ void treinar(){
         }
         printf("\n");
     }
-    getchar();
-
-    //float amostras[4][2] = { {0, 0}, {0, 1}, {1, 0}, {1, 1} };
-
+    //getchar();
 
     preencherMatrizes();
 
     for(numEpocas = 0; numEpocas < EPOCAS; numEpocas++){
-        for(j = 0; j < NUMAMOSTRAS; j++){   //indica a amostra atual
-            //printf("Amostra %d\n\n", j+1);
+        for(j = 0; j < NUMAMOSTRAS; j++){   // Indica a amostra atual
 
-            //FEEDFORWARD
+            /* FEEDFORWARD */
             // OCULTA
-            for(k = 0; k < NUMENTRADAS; k++){   // atribuindo as entradas
+            for(k = 0; k < NUMENTRADAS; k++){   // Recebendo as entradas das amostras
                 entrada[k][0] = amostras[j][k];
                 //printf("%f\n", entrada[k][0]);
             }
 
-            for(k = 0; k < NEUOCULTA; k++){     // fazendo a multiplicação da entrada pelos pesos da oculta
+            for(k = 0; k < NEUOCULTA; k++){     // Multiplicação de pesos da oculta pela entrada
                 soma = 0;
                 for(i = 0; i < NUMENTRADAS; i++){
                     soma += pesosOculta[k][i] * entrada[i][0];
@@ -114,73 +107,57 @@ void treinar(){
                 oculta[k][0] = soma;
                 oculta[k][0] = sigmoide(oculta[k][0]);
                 //printf("oculta = %f\n", oculta[k][0]);
-                //system("pause");
-
             }
 
             // SAIDA
-            for(k = 0; k < NEUSAIDA; k++){     // fazendo a multiplicação da oculta pelos pesos da saida
+            for(k = 0; k < NEUSAIDA; k++){     // Multiplicação de pesos da saida pela oculta
                 soma = 0;
                 for(i = 0; i < NEUOCULTA; i++){
                     soma += pesosSaida[k][i] * oculta[i][0];
-                    //printf("\pesosSaida[%d][%d] = %f | oculta[%d][0] = %f\n", k, i, pesosSaida[k][i], i, oculta[i][0]); //erro aqui [problema com a oculta]
-                    //getchar();
+                    //printf("\pesosSaida[%d][%d] = %f | oculta[%d][0] = %f\n", k, i, pesosSaida[k][i], i, oculta[i][0]);
                 }
                 saida[j][0] = soma;
                 saida[j][0] = sigmoide(saida[j][0]);
                 //printf("saida = %f\n", saida[j][0]);
             }
-            //system("pause");
 
             erroOculta[j][0] = (desejado[j] - saida[j][0]) * dsig(saida[j][0]);  //calculando o erro
             //printf("\nerro[%d][0] = %f | saida[%d][0] = %f | desejado[%d] = %f\n", j, erroOculta[j][0], j, saida[j][0], j, desejado[j]);
-            //getchar();
 
-
-            //BACKPROPAGATION
+            /* BACKPROPAGATION */
 
             //SAIDA
-            for(k = 0; k < NEUOCULTA; k++){     //atualizando os pesos da saida
+            for(k = 0; k < NEUOCULTA; k++){     // Atualiza pesos da Saida
                 //printf("\npesosSaida[0][%d] = %f", k, pesosSaida[0][k]);
                 pesosSaida[0][k] += eta * erroOculta[j][0] * oculta[k][0];
                 //printf("\npesosSaida[0][%d] = %f | erroOculta[%d][0] = %f | oculta[%d][0] = %f", k, pesosSaida[0][k], j, erroOculta[j][0], j, oculta[k][0]);
-                //getchar();
             }
-            //printf("\n--\n");
             //OCULTA
-            for(k = 0; k < NEUOCULTA; k++){     //atualizano os pesos da oculta
+            for(k = 0; k < NEUOCULTA; k++){     // Atualiza pesos da Oculta
                 gradienteOculta[k][0] = erroOculta[j][0] * pesosSaida[0][k] * dsig(oculta[k][0]);
                 //printf("\ngradienteOculta[%d][0] = %f | erroOculta[%d][0] = %f | pesosSaida[0][%d] = %f | dsigoculta[%d][0] = %f", k, gradienteOculta[k][0], j, erroOculta[j][0], k, pesosSaida[0][k], k, dsig(oculta[k][0]));
                 for(i = 0; i < NUMENTRADAS; i++){
                     pesosOculta[k][i] += eta * gradienteOculta[k][0] * entrada[i][0];
                     //printf("\nentrada[%d][0] = %f | pesosOculta[%d][%d] = %f", i, entrada[i][0], k, i, pesosOculta[k][i]);
-                    //getchar();
                 }
             }
         }
-        //printf("\n\n");
 
-        //getchar();
         if(numEpocas == EPOCAS-1){
             for(i = 0; i < NUMAMOSTRAS; i++){
                 for(k = 0; k < NUMENTRADAS; k++){
                     printf("\nEntrada[%d] = \t%f", k, amostras[i][k]);
                 }
-                printf("\n");
-
-                printf("\nSaida Obtida = \t%f", saida[i][0]);
+                printf("\n\nSaida Obtida = \t%f", saida[i][0]);
                 printf("\nSaida Desejado = \t%f", desejado[i]);
                 printf("\nErro = \t%f", erroOculta[i][0]);
                 printf("\n===================\n");
-
-
             }
         }
-        //printf("\n===================\n");
     }// fim do for epocas
 }// fim da funcao treinar
 
-void salvar(){
+int salvarPesos(){
 
     int i, j;
 
@@ -213,12 +190,13 @@ void salvar(){
     fclose(arqPesoOculta);
     fclose(arqPesoSaida);
 
+    return 0;
 }
 
 int main(){
 
     treinar();
-    salvar();
-
-    return 1;
+    salvarPesos();
+    printf("\nRede treinada com sucesso :)\n");
+    return 0;
 }
